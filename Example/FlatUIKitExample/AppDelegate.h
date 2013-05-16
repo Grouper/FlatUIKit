43313b902d8c667de6fdaa315384349773a0e466
Alex Benito <alexbenito@fastmail.fm>
2013-05-16 12:12:55 +0200

    Added example Xcode project Removed reference to missing header

---
+++ Example/FlatUIKitExample/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  FlatUIKitExample
+//
+//  Created by Alejandro Benito Santos on 5/16/13.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ Example/FlatUIKitExample/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  FlatUIKitExample
+//
+//  Created by Alejandro Benito Santos on 5/16/13.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
