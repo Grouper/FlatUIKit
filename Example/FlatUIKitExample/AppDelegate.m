bfe4dad10d3869886019e43def0587caf48b2d95
jflinter <jflinter11@gmail.com>
2013-05-16 11:22:11 -0400

    nav controller to demonstrate navbar

--- Example/FlatUIKitExample/AppDelegate.m
+++ Example/FlatUIKitExample/AppDelegate.m
@@ -17,7 +17,8 @@
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
     self.viewController = [[ViewController alloc] initWithNibName:@"ViewController" bundle:nil];
-    self.window.rootViewController = self.viewController;
+    UINavigationController *navController = [[UINavigationController alloc] initWithRootViewController:self.viewController];
+    self.window.rootViewController = navController;
     [self.window makeKeyAndVisible];
     return YES;
 }
