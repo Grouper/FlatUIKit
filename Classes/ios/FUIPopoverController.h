da7bdff71c2e27945a9868c3507dda7dfcce13d3
Ignacio Romero <iromero@dzen.cl>
2013-05-30 15:36:44 -0400

    Added FUIPopoverController for custom flat iPad popovers

---
+++ Classes/ios/FUIPopoverController.h
@@ -0,0 +1,15 @@
+//
+//  FUIPopoverController.h
+//  FlatComponents
+//
+//  Created by Ignacio on 5/30/13.
+//  Copyright (c) 2013 DZEN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FUIPopoverController : UIPopoverController
+
+@property (nonatomic, assign) UIColor *backgroundColor;
+
+@end
