c0ad91f33326f3fbaf4134d3f583eedefcd75aaa
Adam Fraser <adamfraser0@gmail.com>
2013-06-08 18:59:45 +1000

    Add test helper method category

---
+++ Tests/FUIKitTestsTests/SenTestCase+FUITestHelpers.h
@@ -0,0 +1,14 @@
+//
+//  SenTestCase+FUIKitTestHelpers.h
+//  FUIKitTests
+//
+//  Copyright (c) 2013 Grouper. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface SenTestCase (FUITestHelpers)
+
+- (void)waitForAnimations;
+
+@end
