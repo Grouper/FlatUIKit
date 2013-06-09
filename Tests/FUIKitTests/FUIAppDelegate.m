25fc58a3a76f37ca2b46286318199584e096c616
Adam Fraser <adamfraser0@gmail.com>
2013-06-09 16:48:25 +1000

    Remove unused app delegate code

--- Tests/FUIKitTests/FUIAppDelegate.m
+++ Tests/FUIKitTests/FUIAppDelegate.m
@@ -4,10 +4,6 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-    self.window.backgroundColor = [UIColor whiteColor];
-    [self.window makeKeyAndVisible];
-    self.window.rootViewController = [[UIViewController alloc] init];
     return YES;
 }
 
