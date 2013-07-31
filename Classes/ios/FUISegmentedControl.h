cd955cba31b16f3a64feaf3c38b7de22336eb8bd
Joshua Moody <joshuajmoody@gmail.com>
2013-07-31 18:57:01 +0200

    xcode 4.6 is complaining about:
    No 'assign', 'retain', or 'copy' attribute is specified - 'assign' is
    assumed
    
    on around some properties without explicit strong.

--- Classes/ios/FUISegmentedControl.h
+++ Classes/ios/FUISegmentedControl.h
@@ -10,14 +10,14 @@
 
 @interface FUISegmentedControl : UISegmentedControl
 
-@property(nonatomic, readwrite) UIColor *selectedColor UI_APPEARANCE_SELECTOR;
-@property(nonatomic, readwrite) UIColor *deselectedColor UI_APPEARANCE_SELECTOR;
-@property(nonatomic, readwrite) UIColor *dividerColor UI_APPEARANCE_SELECTOR;
+@property(nonatomic, strong, readwrite) UIColor *selectedColor UI_APPEARANCE_SELECTOR;
+@property(nonatomic, strong, readwrite) UIColor *deselectedColor UI_APPEARANCE_SELECTOR;
+@property(nonatomic, strong, readwrite) UIColor *dividerColor UI_APPEARANCE_SELECTOR;
 @property(nonatomic, readwrite) CGFloat cornerRadius UI_APPEARANCE_SELECTOR;
-@property(nonatomic, readwrite) UIFont *selectedFont UI_APPEARANCE_SELECTOR;
-@property(nonatomic, readwrite) UIFont *deselectedFont UI_APPEARANCE_SELECTOR;
-@property(nonatomic, readwrite) UIColor *selectedFontColor UI_APPEARANCE_SELECTOR;
-@property(nonatomic, readwrite) UIColor *deselectedFontColor UI_APPEARANCE_SELECTOR;
+@property(nonatomic, strong, readwrite) UIFont *selectedFont UI_APPEARANCE_SELECTOR;
+@property(nonatomic, strong, readwrite) UIFont *deselectedFont UI_APPEARANCE_SELECTOR;
+@property(nonatomic, strong, readwrite) UIColor *selectedFontColor UI_APPEARANCE_SELECTOR;
+@property(nonatomic, strong, readwrite) UIColor *deselectedFontColor UI_APPEARANCE_SELECTOR;
 
 
 
