52db1ff6657b3a81bc5ed1f4485adf2a374147f5
jflinter <jflinter11@gmail.com>
2014-04-18 17:59:53 -0400

    error checking around UIFont+FlatUI

--- Classes/ios/UIFont+FlatUI.m
+++ Classes/ios/UIFont+FlatUI.m
@@ -11,36 +11,30 @@
 
 @implementation UIFont (FlatUI)
 
-+ (UIFont *)flatFontOfSize:(CGFloat)size {
++ (void) initialize {
+    [super initialize];
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
-        NSURL * url = [[NSBundle mainBundle] URLForResource:@"Lato-Regular" withExtension:@"ttf"];
-		CFErrorRef error;
-        CTFontManagerRegisterFontsForURL((__bridge CFURLRef)url, kCTFontManagerScopeNone, &error);
-        error = nil;
+        NSArray *fontNames = @[@"Lato-Regular", @"Lato-Bold", @"Lato-Italic"];
+        for (NSString *fontName in fontNames) {
+            NSURL * url = [[NSBundle mainBundle] URLForResource:fontName withExtension:@"ttf"];
+            if (url) {
+                CFErrorRef error;
+                CTFontManagerRegisterFontsForURL((__bridge CFURLRef)url, kCTFontManagerScopeNone, &error);
+            }
+        }
     });
+}
+
++ (UIFont *)flatFontOfSize:(CGFloat)size {
     return [UIFont fontWithName:@"Lato-Regular" size:size];
 }
 
 + (UIFont *)boldFlatFontOfSize:(CGFloat)size {
-    static dispatch_once_t onceToken;
-    dispatch_once(&onceToken, ^{
-        NSURL * url = [[NSBundle mainBundle] URLForResource:@"Lato-Bold" withExtension:@"ttf"];
-		CFErrorRef error;
-        CTFontManagerRegisterFontsForURL((__bridge CFURLRef)url, kCTFontManagerScopeNone, &error);
-        error = nil;
-    });
     return [UIFont fontWithName:@"Lato-Bold" size:size];
 }
 
 + (UIFont *)italicFlatFontOfSize:(CGFloat)size {
-    static dispatch_once_t onceToken;
-    dispatch_once(&onceToken, ^{
-        NSURL * url = [[NSBundle mainBundle] URLForResource:@"Lato-Italic" withExtension:@"ttf"];
-		CFErrorRef error;
-        CTFontManagerRegisterFontsForURL((__bridge CFURLRef)url, kCTFontManagerScopeNone, &error);
-        error = nil;
-    });
     return [UIFont fontWithName:@"Lato-Italic" size:size];
 }
 
