d340f7b993bc369bb4f3243b345a53bc78dbbf4e
linzhongsc <lin.zhong@snapchat.com>
2015-03-10 16:49:51 -0700

    1. Add the interface to make highlighted background configurable. 2. Add two events for touch down and touch up

--- Classes/ios/FUISegmentedControl.h
+++ Classes/ios/FUISegmentedControl.h
@@ -13,14 +13,17 @@
 @property(nonatomic, strong, readwrite) UIColor *selectedColor UI_APPEARANCE_SELECTOR;
 @property(nonatomic, strong, readwrite) UIColor *disabledColor UI_APPEARANCE_SELECTOR;
 @property(nonatomic, strong, readwrite) UIColor *deselectedColor UI_APPEARANCE_SELECTOR;
+@property(nonatomic, strong, readwrite) UIColor *highlightedColor UI_APPEARANCE_SELECTOR;
 @property(nonatomic, strong, readwrite) UIColor *dividerColor UI_APPEARANCE_SELECTOR;
 @property(nonatomic, readwrite) CGFloat cornerRadius UI_APPEARANCE_SELECTOR;
 @property(nonatomic, strong, readwrite) UIFont *selectedFont UI_APPEARANCE_SELECTOR;
 @property(nonatomic, strong, readwrite) UIFont *disabledFont UI_APPEARANCE_SELECTOR;
 @property(nonatomic, strong, readwrite) UIFont *deselectedFont UI_APPEARANCE_SELECTOR;
+@property(nonatomic, strong, readwrite) UIFont *highlightedFont UI_APPEARANCE_SELECTOR;
 @property(nonatomic, strong, readwrite) UIColor *selectedFontColor UI_APPEARANCE_SELECTOR;
 @property(nonatomic, strong, readwrite) UIColor *deselectedFontColor UI_APPEARANCE_SELECTOR;
 @property(nonatomic, strong, readwrite) UIColor *disabledFontColor UI_APPEARANCE_SELECTOR;
+@property(nonatomic, strong, readwrite) UIColor *highlightedFontColor UI_APPEARANCE_SELECTOR;
 @property(nonatomic, strong, readwrite) UIColor *borderColor UI_APPEARANCE_SELECTOR;
 @property(nonatomic, readwrite) CGFloat borderWidth UI_APPEARANCE_SELECTOR;
 
